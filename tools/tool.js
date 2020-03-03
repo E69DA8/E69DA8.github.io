029724d0ddeaea3220c629293c1caf8064b87528
A-S-L <2859787456@qq.com>
2020-03-03 15:02:18 +0800

    添加工具tool.js

---
+++ tools/tool.js
@@ -0,0 +1,63 @@
+//冒泡排序
+function bubble(arr){
+	for(var i = 0; i < arr.length; i++){
+		for(var j = 0; j < arr.length - i - 1; j++){
+			if(arr[j] > arr[j + 1]){
+				var tmp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+}
+
+//选择排序
+function choose(arr){
+	for(var i = 0; i < arr.length -  1; i++){
+		for(var j = i + 1; j < arr.length; j++){
+			if(arr[i]>arr[j]){
+				var tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+}
+
+//获取节点对象
+//格式：id：#id，className：.className,tagName:tagName,name:name=name
+function $(vArg){
+	switch (vArg[0]){
+		case "#"://id
+			return document.getElementById(vArg.substring(1));
+			break;
+		case "."://className
+			return elementByClassName(document,vArg.substring(1));
+			break;
+		default:
+			var str = vArg.substring(0,5);
+			if (str == "name="){//name
+				return document.getElementsByName(vArg.substring(5));
+			}else{//tagName
+				return document.getElementsByTagName(vArg);
+			}
+			break;
+	}
+}
+
+//针对className兼容问题优化
+function elementByClassName(parent,classStr){
+	var nodes = parent.getElementsByTagName("*");//*:通配符，获取所有元素节点
+	var result = [];
+	for(i = 0; i < nodes.length; i++){
+		if(nodes[i].className = classStr){
+			result.push(nodes[i]);
+		}
+	}
+	return result;
+}
+
+//获取当前样式兼容问题优化
+function getStyle(elem,attr){
+	return elem.currentStyle ? elem.currentStyle(attr) : getComputedStyle(elem)[attr];
+}
